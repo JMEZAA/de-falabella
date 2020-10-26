d0624bf5357bdc894e24ecf4504227f4e09d70e3
gmldea gmldea <datox.u@gmail.com>
2020-10-25 23:53:47 -0300

    lectura csv storge con spring boot

--- csv_test/src/main/java/cl/falabella/csv/CsvTestApplication.java
+++ csv_test/src/main/java/cl/falabella/csv/CsvTestApplication.java
@@ -2,11 +2,12 @@ package cl.falabella.csv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.*;
 
 @SpringBootApplication
 public class CsvTestApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(CsvTestApplication.class, args);
 	}
 
--- csv_test/src/main/java/cl/falabella/csv/FileWriterController.java
+++ csv_test/src/main/java/cl/falabella/csv/FileWriterController.java
@@ -1,32 +1,31 @@
 package cl.falabella.csv;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.nio.charset.Charset;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FileWriterController {
-
+	
 @Value("gs://de-falabella/CSV_INPUT/GlobalLandTemperaturesByMajorCity.csv")
 private Resource gcsFile;
 
-	// Metodo para filtrar la lista
+	/*Metodo_para_filtrar_la_lista*/
 	public static List<archivo> ListaFiltrada(List<archivo> list, String filtro) {
 	return list.stream().filter(x -> x.Item5().equals(filtro)).collect(Collectors.toList());
 	}
-	
+
 	public static String MD5(String md5) {
 	    try {
 	        java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
@@ -41,16 +40,16 @@ private Resource gcsFile;
 	    }
 	    return null;
 	}
-	
-	public static void csvFile() throws IOException {
 
-		System.out.println("entre");
+	@RequestMapping(value ="/", method = RequestMethod.GET)	
+	public void csvFile() throws IOException {
+
 		BufferedReader br = null;
 		BufferedReader lr = null;
 		String SEPARATOR = ",";
 		String line = "";
 		FileWriter fichero = null;
-		PrintWriter pw = null;     
+		PrintWriter pw = null;
 
 		ArrayList<String> al = new ArrayList<>();
 		ArrayList<archivo> lista = new ArrayList<>();
@@ -58,12 +57,12 @@ private Resource gcsFile;
 		
         try {
             
-            br =new BufferedReader(new FileReader("gs://de-falabella/CSV_INPUT/GlobalLandTemperaturesByMajorCity.csv"));
+        	br = new BufferedReader(new InputStreamReader( gcsFile.getInputStream()));
             lr = br;
             while((line = br.readLine()) != null){ 
                 String[] datos = line.split(SEPARATOR);                
                 archivo arc = new archivo();   
-                
+
                 if (datos[0].equals("dt")) {
                 	cabecera.Item1(datos[0]);
                 	cabecera.Item2(datos[1]);
@@ -86,15 +85,19 @@ private Resource gcsFile;
                         al.add(datos[4]);                    
                     }
                 }
-            }  
+            } 
+            
+            /*try (OutputStream os = ((WritableResource) gcsFile2).getOutputStream()) {
+            	  os.write("fxx".getBytes());
+            	}*/
             
             for(String a : al) {
             	try {
             		               
-            		fichero = new FileWriter("gs://de-falabella/CSV_OUTPUT/Archivo_Pais_\" + a + \".csv");
+            		fichero = new FileWriter("Archivo_Pais_" + a + ".csv");
             		pw = new PrintWriter(fichero);  
+            		
             		List<archivo> lstfilt = ListaFiltrada(lista, a);
-            		//System.out.println("Total registros " + lstfilt.size());
             		pw.println(cabecera.Item1() + "," + cabecera.Item2() + "," + cabecera.Item3() + "," + cabecera.Item4() + "," + cabecera.Item5() + "," + cabecera.Item6() + "," + cabecera.Item7());
             		
             		for(archivo l : lstfilt) {
@@ -106,6 +109,7 @@ private Resource gcsFile;
             	catch(IOException ex) {
             		System.out.println(ex.getMessage());
             	}
+            	
             }
             
         } catch (IOException e) {
@@ -115,11 +119,6 @@ private Resource gcsFile;
                 br.close();
             }
         }
-		
+
 	}
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-  	public String readGcsFile() throws IOException {
-		return StreamUtils.copyToString(gcsFile.getInputStream(),Charset.defaultCharset()) + "\n";
-  }
 }
